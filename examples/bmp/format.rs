fe435241879c3d77e48008aa300a4959b5ab9b31
elast0ny <elast0ny00@gmail.com>
2020-12-23 21:36:38 -0500

    Added documentation

--- examples/bmp/format.rs
+++ examples/bmp/format.rs
@@ -1,22 +1,24 @@
-use simple_parse::{SpRead, SpWrite, SpError, SpCtx, validate_reader_exact};
+use simple_parse::{validate_reader_exact, SpCtx, SpError, SpRead, SpWrite};
 use std::io::{Read, Write};
 
 #[derive(Debug, SpRead, SpWrite)]
-#[sp(endian = "little")] // The BMP format forces little endian
+#[sp(endian = "little")] // The BMP format explicitely needs little endian
 pub struct BmpHeader {
-    #[sp(validate="validate_magic_header")]
+    #[sp(validate = "validate_magic_header")]
+    // Call `validate_magic_header()` with the contents of `magic`
     pub magic: u16,
     pub size: u32,
     reserved1: u16,
     reserved2: u16,
     pixel_array_offset: u32,
     #[sp(var_size)]
+    // We must tell simple_parse that this custom type has a variable size or this wont compile
     dib: DIBHeader,
 }
 
 // You differentiate DIB header types by their size...
 #[derive(Debug, SpRead, SpWrite)]
-#[sp(id_type = "u32", endian = "little")] // The BMP format forces little endian
+#[sp(id_type = "u32", endian = "little")] // The header size is a u32
 pub enum DIBHeader {
     // Only support BITMAPINFOHEADER for this toy example
     #[sp(id = 40)]
@@ -31,10 +33,9 @@ pub enum DIBHeader {
         vertical_res: i32,
         clr_used: u32,
         important: u32,
-        // The logic for parsing the two fields bellow is complicated. We must use custom readers.
-        // These custom fns can be abstract trait fns
+        // The logic for parsing the two fields bellow is complicated. We must use custom reader/writer
         #[sp(
-            reader="BitmapCompression::read, compression",
+            reader="BitmapCompression::read, compression", // will generate : `self.compression_info = BitmapCompression::read(&compression, ...)?;`
             writer="BitmapCompression::write"
         )]
         compression_info: BitmapCompression,
@@ -50,17 +51,17 @@ pub enum DIBHeader {
 #[derive(Debug)]
 pub enum BitmapCompression {
     None,
-    BitFields{
+    BitFields {
         red: u32,
         green: u32,
         blue: u32,
     },
-    AlphaBitFields{
+    AlphaBitFields {
         alpha: u32,
         red: u32,
         green: u32,
         blue: u32,
-    }
+    },
 }
 
 /// holds a color from the bmp color table
@@ -74,25 +75,28 @@ pub struct RgbQuad {
 
 impl BitmapCompression {
     // Custom SpRead parser based on the compression value
-    fn read<R: Read + ?Sized>(compression_bitmask: &u32, src: &mut R, ctx: &mut SpCtx) -> Result<Self, SpError> {
+    fn read<R: Read + ?Sized>(
+        compression_bitmask: &u32,
+        src: &mut R,
+        ctx: &mut SpCtx,
+    ) -> Result<Self, SpError> {
         const BI_RGB: u32 = 0;
-        const BI_BITFIELDS : u32 = 3;
+        const BI_BITFIELDS: u32 = 3;
         const BI_ALPHABITFIELDS: u32 = 6;
         // The compression method is encoded into the compression header field
         match *compression_bitmask {
-            BI_RGB => {
-                Ok(Self::None)
-            },
+            BI_RGB => Ok(Self::None),
             BI_BITFIELDS => {
                 // Call read() for every u32
                 let red = u32::inner_from_reader(src, ctx)?;
                 let green = u32::inner_from_reader(src, ctx)?;
                 let blue = u32::inner_from_reader(src, ctx)?;
-                Ok(Self::BitFields{red, green, blue})
-            },
+                Ok(Self::BitFields { red, green, blue })
+            }
             BI_ALPHABITFIELDS => {
                 // Call read() once for 16 bytes
                 let mut tmp = [0u8, 16];
+                // Use simple_parse provided helper
                 validate_reader_exact(&mut tmp, src)?;
                 let mut checked_bytes = tmp.as_mut_ptr();
                 let alpha: u32;
@@ -109,32 +113,45 @@ impl BitmapCompression {
                     checked_bytes = checked_bytes.add(4);
                     blue = u32::inner_from_reader_unchecked(checked_bytes, src, ctx)?;
                 }
-                Ok(Self::AlphaBitFields{alpha,red, green, blue})
-            },
-            _ => Err(SpError::InvalidBytes)
+                Ok(Self::AlphaBitFields {
+                    alpha,
+                    red,
+                    green,
+                    blue,
+                })
+            }
+            _ => Err(SpError::InvalidBytes),
         }
     }
     // Custom SpWrite
-    fn write<W: Write + ?Sized>(this: &BitmapCompression, ctx: &mut SpCtx, dst: &mut W) -> Result<usize, SpError> {
+    fn write<W: Write + ?Sized>(
+        this: &BitmapCompression,
+        ctx: &mut SpCtx,
+        dst: &mut W,
+    ) -> Result<usize, SpError> {
         match this {
             &Self::None => Ok(0),
-            &Self::AlphaBitFields{alpha, red, green, blue} => {
+            &Self::AlphaBitFields {
+                alpha,
+                red,
+                green,
+                blue,
+            } => {
                 alpha.inner_to_writer(ctx, dst)?;
                 red.inner_to_writer(ctx, dst)?;
                 green.inner_to_writer(ctx, dst)?;
                 blue.inner_to_writer(ctx, dst)?;
                 Ok(16)
-            },
-            &Self::BitFields{red, green, blue} => {
+            }
+            &Self::BitFields { red, green, blue } => {
                 red.inner_to_writer(ctx, dst)?;
                 green.inner_to_writer(ctx, dst)?;
                 blue.inner_to_writer(ctx, dst)?;
                 Ok(12)
-            },
+            }
         }
     }
 }
-
 
 /// This function is called as soon as the magic u16 byte is read
 fn validate_magic_header(magic: &u16, _ctx: &mut SpCtx) -> Result<(), SpError> {
@@ -147,7 +164,13 @@ fn validate_magic_header(magic: &u16, _ctx: &mut SpCtx) -> Result<(), SpError> {
 }
 
 /// Parses a BMP color table based on header values
-fn parse_color_table<R: Read + ?Sized>(clr_used: &u32, compression: &BitmapCompression, bit_count: &u16, src: &mut R, ctx: &mut SpCtx) -> Result<Vec<RgbQuad>, SpError> {
+fn parse_color_table<R: Read + ?Sized>(
+    clr_used: &u32,
+    compression: &BitmapCompression,
+    bit_count: &u16,
+    src: &mut R,
+    ctx: &mut SpCtx,
+) -> Result<Vec<RgbQuad>, SpError> {
     // The bitmap is not compressed which means every pixel contains the actual color information.
     // The Color table is supposed to be empty
     if let BitmapCompression::None = compression {
@@ -162,7 +185,7 @@ fn parse_color_table<R: Read + ?Sized>(clr_used: &u32, compression: &BitmapCompr
             for _i in 0..(2u8).pow(*bit_count as _) {
                 res.push(RgbQuad::inner_from_reader(src, ctx)?);
             }
-        },
+        }
         // 4 bit_count and up encode how many colors are used in clr_used. This value has a max of 2^bit_count
         4 | 8 | 16 | 24 | 32 => {
             // Make sure not too many colors are provided
@@ -173,7 +196,7 @@ fn parse_color_table<R: Read + ?Sized>(clr_used: &u32, compression: &BitmapCompr
             for _i in 0..*clr_used {
                 res.push(RgbQuad::inner_from_reader(src, ctx)?);
             }
-        },
+        }
         _ => return Err(SpError::InvalidBytes),
     }
 
@@ -181,7 +204,11 @@ fn parse_color_table<R: Read + ?Sized>(clr_used: &u32, compression: &BitmapCompr
 }
 
 /// Writes BMP a color table
-fn write_color_table<W: Write + ?Sized>(this: &Vec<RgbQuad>, ctx: &mut SpCtx, dst: &mut W) -> Result<usize, SpError> {
+fn write_color_table<W: Write + ?Sized>(
+    this: &Vec<RgbQuad>,
+    ctx: &mut SpCtx,
+    dst: &mut W,
+) -> Result<usize, SpError> {
     let mut size_written = 0;
     // extra validation could be done here to ensure clr_used and bit_count is valid for these colors
     for color in this.iter() {
